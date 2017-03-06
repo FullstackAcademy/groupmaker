57792e172a83cfc16e5403fd4a56156fbf7461ca
Jean Chung <jxchung@gmail.com>
2017-03-06 17:18:28 -0600

    webpack compiles now

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -4,12 +4,20 @@ import { connect } from 'react-redux';
 import OneStudent from './OneStudent';
 
 class Form extends Component {
+  onSubmit() {
+    // Send data to database & display thank-you message
+    return;
+  }
+
   render() {
     const { name, students } = this.props.cohort;
     return (
       <div>
-        {name ? <h3>{name}</h3> : null}
-        {students.map(student => <OneStudent name={student} />)}
+        {name ? <h2>{name}</h2> : null}
+        <form>
+          {students.map(student => <OneStudent name={student} />)}
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   }
